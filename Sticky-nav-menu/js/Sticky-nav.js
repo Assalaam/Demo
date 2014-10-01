ca0891936d898f1e57478c11e13ca412aa714d9e
Assalaam <atas_nama_kasihsayang@yahoo.com>
2014-10-01 09:31:43 +0700

    Create Sticky-nav.js

---
+++ Sticky-nav-menu/js/Sticky-nav.js
@@ -0,0 +1,21 @@
+$(function() {
+	var sticky_navigation_offset_top = $('#sticky_navigation').offset().top;
+	var sticky_navigation = function(){
+		var scroll_top = $(window).scrollTop(); // our current vertical position from the top
+		if (scroll_top > sticky_navigation_offset_top) { 
+			$('#sticky_navigation').css({ 'position': 'fixed', 'top':0, 'left':0 });
+		} else {
+			$('#sticky_navigation').css({ 'position': 'relative' }); 
+		}   
+	};
+	sticky_navigation();
+	$(window).scroll(function() {
+		 sticky_navigation();
+	});
+	
+	// NOT required:
+	// for this demo disable all links that point to "#"
+	$('a[href="#"]').click(function(event){ 
+		event.preventDefault(); 
+	});
+});
