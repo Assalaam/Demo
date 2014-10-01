3d18db966938c77c09830927d4ef3d5c161aa204
Assalaam <atas_nama_kasihsayang@yahoo.com>
2014-10-01 09:34:34 +0700

    Update Sticky-nav.js

--- Sticky-nav-menu/js/Sticky-nav.js
+++ Sticky-nav-menu/js/Sticky-nav.js
@@ -1,11 +1,11 @@
 $(function() {
-	var sticky_navigation_offset_top = $('#sticky_navigation').offset().top;
+	var sticky_navigation_offset_top = $('#sticky_nav').offset().top;
 	var sticky_navigation = function(){
-		var scroll_top = $(window).scrollTop(); // our current vertical position from the top
+		var scroll_top = $(window).scrollTop();
 		if (scroll_top > sticky_navigation_offset_top) { 
-			$('#sticky_navigation').css({ 'position': 'fixed', 'top':0, 'left':0 });
+			$('#sticky_nav').css({ 'position': 'fixed', 'top':0, 'left':0 });
 		} else {
-			$('#sticky_navigation').css({ 'position': 'relative' }); 
+			$('#sticky_nav').css({ 'position': 'relative' }); 
 		}   
 	};
 	sticky_navigation();
